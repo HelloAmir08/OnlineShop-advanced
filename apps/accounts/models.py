52295ce48aad90aa1c89a16c5deeb3da068cb558
HelloAmir08 <amirjontoirov03@gmail.com>
2026-07-02 22:40:16 +0500

    account app is partly done

---
+++ apps/accounts/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CASCADE
+from phonenumber_field.modelfields import PhoneNumberField
+from apps.core.models import BaseModel
+from .managers import UserManager
+
+class User(AbstractUser):
+    phone_number = PhoneNumberField(
+        region = "UZ",
+        unique = True
+    )
+    email = models.EmailField(
+        blank = True,
+        null = True
+    )
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.phone_number.as_e164
+
+class Profile(BaseModel):
+    avatar = models.ImageField(
+        upload_to='profile_image/',
+        blank = True,
+        null = True,
+        default= 'profile_image/avatar-default.svg'
+    )
+    birthday = models.DateField(
+        blank=True,
+        null = True
+    )
+    address = models.TextField(
+        blank = True,
+        null = True
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete = CASCADE,
+        related_name = 'profile'
+    )
+
+    def __str__(self):
+        return f'{self.user.phone_number} - Profile'
+
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.core.apps.CoreConfig',
     'phonenumber_field',
+    'apps.accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = "accounts.User"
